11de33023c73f7245a73a425189ee0a71fc8d8e3
Carlos Henrique <carlos_henrique1313@outlook.com>
2023-04-24 21:23:31 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/authkotlin/AuthKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.authkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthKotlinApplication>(*args)
+}
